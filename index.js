b347c508363bd8a3bda64bf8dace0aa0bcfd3f6a
unknown <pirjademl7@gmail.com>
2023-11-10 19:48:49 +0530

    Added certificates

--- index.js
+++ index.js
@@ -6,3 +6,39 @@ toggleButton.addEventListener("click", (event) => {
   event.preventDefault(); // Prevent the default behavior of the link
   navbar.classList.toggle("active");
 });
+function sendEmail() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  const data = {
+    name: name,
+    email: email,
+    message: message,
+  };
+
+  fetch("https://formspree.io/pirjademl7@gmail.com", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        alert("Message sent successfully!");
+      } else {
+        alert("Message failed to send. Please try again later.");
+      }
+    });
+}
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent the default form submission
+
+    sendEmail();
+  });
