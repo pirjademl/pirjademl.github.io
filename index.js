75a2ea4d8beec54ed6d64132c2f1e34b5cbf659c
unknown <pirjademl7@gmail.com>
2023-11-25 19:00:58 +0530

    Added Scroll Behaviour

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const toggleButton = document.getElementById("menu-toggle");
 const navbar = document.getElementById("menu-links");
 
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+});
+
 toggleButton.addEventListener("click", (event) => {
   event.preventDefault(); // Prevent the default behavior of the link
   navbar.classList.toggle("active");
@@ -10,8 +22,6 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
     }
   });
 });
